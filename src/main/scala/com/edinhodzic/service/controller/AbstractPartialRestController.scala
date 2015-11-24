31ea3e588cd671baad2ed5a26b4139c20f263999
edinhodzic <edin.hodzic@dods.co.uk>
2015-11-24 23:28:59 +0000

    refactored controller to avoid repetition

--- src/main/scala/com/edinhodzic/service/controller/AbstractPartialRestController.scala
+++ src/main/scala/com/edinhodzic/service/controller/AbstractPartialRestController.scala
@@ -11,7 +11,7 @@ import com.edinhodzic.service.repository.AbstractPartialCrudRepository
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.language.postfixOps
-import scala.util.{Failure, Success}
+import scala.util.{Try, Failure, Success}
 
 abstract class AbstractPartialRestController[T <: Identifiable : Manifest](abstractCrudRepository: AbstractPartialCrudRepository[T]) {
 
@@ -22,12 +22,8 @@ abstract class AbstractPartialRestController[T <: Identifiable : Manifest](abstr
   @Produces(Array(APPLICATION_JSON))
   def post(resource: T): Response = {
     logger info s"posting $resource"
-    abstractCrudRepository create resource match {
-      case Success(subject) => Response created uri(subject) entity subject build()
-      case Failure(throwable) =>
-        logger error s"$throwable"
-        Response serverError() build()
-    }
+    process[T](abstractCrudRepository create resource,
+      t => Response created uri(t) entity t build())
   }
 
   @GET
@@ -35,16 +31,10 @@ abstract class AbstractPartialRestController[T <: Identifiable : Manifest](abstr
   @Produces(Array(APPLICATION_JSON))
   def get(@PathParam("resourceId") resourceId: String): Response = {
     logger info s"getting $resourceId"
-    abstractCrudRepository read resourceId match {
-      case Success(maybeResource) => maybeResource match {
-        case Some(resource) => Response ok() entity resource build()
-        case None => Response status NOT_FOUND build()
-      }
-      // TODO address pattern matching repetition between this and the post function above
-      case Failure(throwable) =>
-        logger error s"$throwable"
-        Response serverError() build()
-    }
+    process[Option[T]](abstractCrudRepository read resourceId, {
+      case Some(resource) => Response ok() entity resource build()
+      case None => Response status NOT_FOUND build()
+    })
   }
 
   @PUT
@@ -61,4 +51,11 @@ abstract class AbstractPartialRestController[T <: Identifiable : Manifest](abstr
   private def uri(resource: T)(implicit manifest: Manifest[T]): URI =
     new URI(s"${manifest.runtimeClass.getSimpleName.toLowerCase}/${resource id}")
 
+  private def process[S](repositoryFunction: (Try[S]), successFunction: (S => Response)): Response =
+    repositoryFunction match {
+      case Success(subject) => successFunction(subject)
+      case Failure(throwable) =>
+        logger error s"$throwable"
+        Response serverError() build()
+    }
 }
